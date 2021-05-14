caea6cb7d7b31ab3df21d20ff5f8412d44c8c32b
Aio <AioDev.JS@gmail.com>
2021-05-14 19:45:01 +0900

    #2 PostImages 구현

--- components/PostImages.js
+++ components/PostImages.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
+
 const PostImages = ({ images }) => {
-  return <div>구현중</div>;
-};
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  // image가 0개 일때는 부모 컴포넌트에서 postImage를 불러오지 않게 처리해서 분기 처리 안 해됨.
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          style={{ maxHeight: '200px' }}
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
 
-PostImages.propTypes = {
-  iamges: PropTypes.arrayOf(PropTypes.object),
-};
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          style={{ width: '50%', display: 'inline-block' }}
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          style={{ width: '50%', display: 'inline-block' }}
+          role="presentation"
+          alt={images[1].src}
+          src={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
 
+  return (
+    <>
+      <div>
+        <img
+          style={{ width: '50%', display: 'inline-block' }}
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: 'inline-block',
+            width: '50%',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}개의 사진 더 보기
+        </div>
+      </div>
+    </>
+  );
+};
 export default PostImages;
